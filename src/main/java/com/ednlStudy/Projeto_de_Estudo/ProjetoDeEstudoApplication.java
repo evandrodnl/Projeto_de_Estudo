49a49adb972734bf0de5bcf7221d185bd39f046f
Evandro DNL <evandrodnl@gmail.com>
2025-05-22 12:12:23 -0300

    Projeto criado

---
+++ src/main/java/com/ednlStudy/Projeto_de_Estudo/ProjetoDeEstudoApplication.java
@@ -0,0 +1,13 @@
+package com.ednlStudy.Projeto_de_Estudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDeEstudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDeEstudoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ednlStudy/Projeto_de_Estudo/ProjetoDeEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ednlStudy.Projeto_de_Estudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDeEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
